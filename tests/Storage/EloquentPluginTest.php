ef0b130eda37e5fcec183ac1114f3f4671b45189
Don B. Stringham <me@donbstringham.us>
2017-11-22 19:08:47 -0700

    Added EloquenPlugin test

---
+++ tests/Storage/EloquentPluginTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stringhamdb
+ * Date: 11/15/17
+ * Time: 6:54 PM
+ */
+
+namespace Tests\Storage;
+
+use App\Storage\EloquentPlugin;
+use Doctrine\Instantiator\Exception\UnexpectedValueException;
+use Illuminate\Database\Capsule\Manager;
+
+class TestEloquentPlugin extends \PHPUnit_Framework_TestCase {
+    /** @var $db Manager **/
+    protected $db;
+
+    public function setUp() {
+        $this->db = new Manager();
+        $this->db->addConnection(array(
+            'driver'    => 'mysql',
+            'host'      => '165.227.109.168',
+            'database'  => 'web4350',
+            'username'  => 'appuser',
+            'password'  => 'letmein',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ));
+        $this->db->bootEloquent();
+        $this->db->setAsGlobal();
+
+        $this->db::statement('CREATE TABLE IF NOT EXISTS test_table(
+            id INT(11) NOT NULL AUTO_INCREMENT,
+            uuid VARCHAR(36),
+            data VARCHAR(255),
+            PRIMARY KEY (id))');
+    }
+
+    public function tearDown() {
+        $this->db::statement('DROP TABLE test_table');
+    }
+
+    public function testConstructor() {
+        // arrange and act
+        $harness = new EloquentPlugin($this->db->table('test_table'));
+        // assert
+        $this->assertEquals(EloquentPlugin::class, get_class($harness));
+    }
+
+    public function testCreateObject00() {
+        // arrange
+        $expected = 1;
+        $harness = new EloquentPlugin($this->db->table('test_table'));
+        // act
+        $item = new TestClass00('1234abcd', 'foobar');
+        $actual = $harness->Create($item);
+        // assert
+        $this->assertEquals($expected, $actual);
+    }
+}
+
